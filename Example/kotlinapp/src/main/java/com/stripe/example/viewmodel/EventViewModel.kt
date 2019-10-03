eab4d2a0d6c3876411ff5d8d903c2d927331fea0
danwaters-stripe <37308241+danwaters-stripe@users.noreply.github.com>
2019-10-03 13:35:23 -0700

    Release 1.0.0-rc2 (#71)
    * Release 1.0.0-rc2
    
    * Lint

--- Example/kotlinapp/src/main/java/com/stripe/example/viewmodel/EventViewModel.kt
+++ Example/kotlinapp/src/main/java/com/stripe/example/viewmodel/EventViewModel.kt
@@ -7,10 +7,8 @@ import com.stripe.stripeterminal.callable.Cancelable
 
 class EventViewModel constructor(eventsList: List<Event> = mutableListOf()) : ViewModel() {
     private val eventList: ArrayList<Event> = ArrayList(eventsList)
-    var events: MutableLiveData<List<Event>> =
-            MutableLiveData<List<Event>>().apply { value = eventsList }
-    var isComplete: MutableLiveData<Boolean> =
-            MutableLiveData<Boolean>().apply { value = false }
+    var events: MutableLiveData<List<Event>> = MutableLiveData(eventList)
+    var isComplete: MutableLiveData<Boolean> = MutableLiveData(false)
     var collectTask: Cancelable? = null
 
     fun addEvent(event: Event) {
