676a26e16f6235298d4b0a103d4b36f414ff64f6
Shirley Gaw <sgaw@stripe.com>
2021-02-17 18:22:00 -0800

    Use alert boxes instead of a runtime exception crashd so the user has the information without looking at logcat

--- Example/kotlinapp/src/main/java/com/stripe/example/MainActivity.kt
+++ Example/kotlinapp/src/main/java/com/stripe/example/MainActivity.kt
@@ -44,9 +44,13 @@ class MainActivity : AppCompatActivity(), NavigationListener {
 
         // Check that the example app has been configured correctly
         if (ApiClient.BACKEND_URL.isEmpty()) {
-            throw RuntimeException("You need to set the BACKEND_URL constant in ApiClient.kt " +
-                    "before you'll be able to use the example app.")
-        }
+            // notify user
+            AlertDialog.Builder(ContextThemeWrapper(this, R.style.Theme_MaterialComponents_DayNight_DarkActionBar))
+                    .setMessage("You need to set the BACKEND_URL constant in ApiClient.kt " +
+                            "before you'll be able to use the example app.")
+                    .setCancelable(false)
+                    .create()
+                    .show() }
 
         if (BluetoothAdapter.getDefaultAdapter()?.isEnabled == false) {
             BluetoothAdapter.getDefaultAdapter().enable()
